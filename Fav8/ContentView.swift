623e157be8cca60fc31d0310d805192c9c7fcb64
vzoot_author <vzoot@vzoot.com>
2022-11-15 18:35:43 +0100

    Update xcode project

--- Fav8/ContentView.swift
+++ Fav8/ContentView.swift
@@ -5,15 +5,49 @@
 //  Created by Administrator on 11/13/22.
 //
 
+import Combine
+import RadioStationsAPI
 import SwiftUI
 
+struct RadioStation: Identifiable {
+    var id = UUID()
+    var name: String
+    var url: String
+}
+
+class ViewModel: ObservableObject {
+    @Published var radioStations: [RadioStation] = []
+
+    var cancelables = Set<AnyCancellable>()
+
+    init() {
+        fetchStations()
+            .sink(receiveCompletion: { completion in
+                print(completion)
+            }, receiveValue: { radioStations in
+                self.radioStations = radioStations
+            })
+            .store(in: &cancelables)
+    }
+
+    func fetchStations() -> AnyPublisher<[RadioStation], Error> {
+        StationsAPI.getAllRadioStations()
+            .map { $0.map { RadioStation(name: $0.name , url: $0.url ) }}
+            .eraseToAnyPublisher()
+    }
+}
+
 struct ContentView: View {
+    @StateObject var vm: ViewModel = .init()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ForEach(vm.radioStations) { station in
+                HStack {
+                    Text(station.name)
+                    Text(station.url)
+                }
+            }
         }
         .padding()
     }
